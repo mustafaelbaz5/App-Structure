3043e293b3925ec7b4ce4b81b6b6ec1a443d44e8
Mustafa Elbaz <m9stafa05@gmail.com>
2025-12-04 21:25:28 +0200

    Refactor project structure by removing unused dependencies and updating configuration files. Added launch configurations for development and production environments, and improved documentation in pubspec.yaml. Updated Android and iOS plugin registrations to streamline the build process.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
+    id 'com.android.application'
+    id 'kotlin-android'
+    // Flutter Gradle Plugin must come after Android and Kotlin plugins
+    id 'dev.flutter.flutter-gradle-plugin'
 }
 
 android {
@@ -10,31 +10,44 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.flutter_structure"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Signing config (debug for now, replace with release config later)
             signingConfig = signingConfigs.getByName("debug")
+        }
+        debug {
+            // Optional: debug-specific settings
+        }
+    }
+
+    flavorDimensions += "default"
+    productFlavors {
+        development {
+            dimension = "default"
+            resValue "string", "app_name", "K Development"
+            applicationIdSuffix = ".development"
+        }
+        production {
+            dimension = "default"
+            resValue "string", "app_name", "K Production"
+            applicationIdSuffix = ".production"
         }
     }
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,11 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  app_links
-  firebase_core
-  flutter_inappwebview_windows
   flutter_secure_storage_windows
-  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
