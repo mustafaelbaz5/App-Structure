bb52f588570c734b18b352f17f5d94eb92a83f73
mustafaelbaz5 <m9stafa05@gmail.com>
2025-12-11 18:44:09 +0200

    remove font family from the app text styles to use the font family of the app theme

--- lib/core/themes/app_text_styles.dart
+++ lib/core/themes/app_text_styles.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_structure/core/themes/app_font_family.dart';
 
 import 'app_font_weight.dart';
 
 class AppTextStyles {
   const AppTextStyles._();
-  static const TextStyle font32ManropeBold = TextStyle(
+  static const TextStyle font32Bold = TextStyle(
     fontSize: 32,
-    fontFamily: AppFontFamily.manrope,
     fontWeight: AppFontWeight.bold,
   );
-  static const TextStyle font16ManropeRegular = TextStyle(
+  static const TextStyle font16Regular = TextStyle(
     fontSize: 16,
-    fontFamily: AppFontFamily.manrope,
     fontWeight: AppFontWeight.regular,
   );
 }
